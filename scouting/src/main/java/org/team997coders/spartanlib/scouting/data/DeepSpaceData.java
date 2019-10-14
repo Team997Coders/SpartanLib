7bf5ab4330e3217904ce5d36beca4d8dfe1a62a1
KyroVibe <hunter.barclay18@gmail.com>
2019-10-14 16:22:35 -0700

    More fixing

--- scouting/src/main/java/org/team997coders/spartanlib/scouting/data/DeepSpaceData.java
+++ scouting/src/main/java/org/team997coders/spartanlib/scouting/data/DeepSpaceData.java
@@ -2,9 +2,23 @@ package org.team997coders.spartanlib.scouting.data;
 
 public class DeepSpaceData implements GameData {
 
-  public int lowCargo, midCargo, highCargo, csCargo,
-    lowHatch, midHatch, highHatch,
-    climb, autoHab, defense, penalty;
-  public Color alliance;
+  public int teamNumber, matchNumber, lowCargo, midCargo, highCargo, csCargo, lowHatch, midHatch, highHatch, climb,
+      autoHab, defense, penalty;
+
+  public Color alliance = Color.Blue;
+
+  public enum Color {
+    Red(0), Blue(1);
+
+    int value;
+
+    Color(int v) {
+      value = v;
+    }
+
+    public int getValue() {
+      return value;
+    }
+  }
 
 }
--- scouting/src/main/java/org/team997coders/spartanlib/scouting/data/GameData.java
+++ scouting/src/main/java/org/team997coders/spartanlib/scouting/data/GameData.java
@@ -1,19 +1,3 @@
 package org.team997coders.spartanlib.scouting.data;
 
-public interface GameData {
-
-  public int teamNumber = 0, matchNumber = 0;
-  public Color alliance = Color.Blue;
-
-  public enum Color {
-    Red(0), Blue(1);
-
-    int value;
-
-    Color(int v) {
-      value = v;
-    }
-
-    public int getValue() { return value; }
-  }
-}
+public interface GameData { }
