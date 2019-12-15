3331605acd9b384c29ddf37c60d90cbbf954c550
KyroVibe <hunter.barclay18@gmail.com>
2019-12-15 00:59:19 -0800

    Made new stuff

---
+++ drivetrain/src/main/java/org/team997coders/spartanlib/oi/controllers/Gamepad.java
@@ -0,0 +1,3 @@
+package org.team997coders.spartanlib.oi.controllers;
+
+public interface Gamepad { }
---
+++ drivetrain/src/main/java/org/team997coders/spartanlib/oi/controllers/Logitech.java
@@ -0,0 +1,63 @@
+package org.team997coders.spartanlib.oi.controllers;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * Basic command-based wrapper class for Logitech controllers
+ */
+public class Logitech implements Gamepad {
+
+  public Joystick mGamepad;
+
+  public JoystickButton
+    mA, mB, mX, mY,
+    mLeftBumper, mRightBumper,
+    mStart, mBack, mLeftStick, mRightStick;
+
+  public Logitech(int port) {
+
+    mGamepad = new Joystick(port);
+
+    mA = new JoystickButton(mGamepad, Buttons.A);
+    mB = new JoystickButton(mGamepad, Buttons.B);
+    mX = new JoystickButton(mGamepad, Buttons.X);
+    mY = new JoystickButton(mGamepad, Buttons.Y);
+    mLeftBumper = new JoystickButton(mGamepad, Buttons.LEFT_BUMPER);
+    mRightBumper = new JoystickButton(mGamepad, Buttons.RIGHT_BUMPER);
+    mBack = new JoystickButton(mGamepad, Buttons.BACK);
+    mStart = new JoystickButton(mGamepad, Buttons.START);
+    mLeftStick = new JoystickButton(mGamepad, Buttons.LEFT_STICK);
+    mRightStick = new JoystickButton(mGamepad, Buttons.RIGHT_STICK);
+  }
+
+  public double getAxis(int port) {
+    return mGamepad.getRawAxis(port);
+  }
+
+  public static class Buttons {
+
+    public static final int
+
+      A = 1, B = 2, X = 3, Y = 4,
+      LEFT_BUMPER = 5, RIGHT_BUMPER = 6,
+      BACK = 7, START = 8,
+      LEFT_STICK = 9, RIGHT_STICK = 10;
+
+  }
+
+  public static class Axis {
+
+    public static final int
+
+      LEFT_X = 0,
+      LEFT_Y = 1,
+      RIGHT_X = 4,
+      RIGHT_Y = 5,
+
+      LEFT_TRIGGER = 2,
+      RIGHT_TRIGGER = 3;
+
+  }
+
+}
